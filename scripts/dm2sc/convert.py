ab5955965ce6f2951ebfb3f562e74a5e55bdbe40
jeanim <jeanimator@gmail.com>
2022-08-03 11:51:58 +0800

    opt code format

--- scripts/dm2sc/convert.py
+++ scripts/dm2sc/convert.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 #
 # Author: Godfrey Huang
-# Email: godfreyhuang@cgenter.com
-# Website: www.cgenter.com
+# Email: godfrey.huang@udecg.com
+# Website: www.udecg.com
 #
 # Created by Godfrey Huang on 2015-11-02.
 # Copyright (c) 2015 CGENTER.COM. All rights reserved.
@@ -11,32 +11,34 @@
 import pymel.core as pm
 
 def batchCovert():
-	sels = pm.selected()
-	if sels:
-		for sel in sels:
-			if sel.getShape().type() == 'mesh':
-				originNode = sel
-				originShapeNodes = originNode.getShapes()
-				pm.select(cl=True)
-				if originShapeNodes:
-					originShapeNode = originShapeNodes[0]
-					historyNodes = originShapeNode.listHistory()
-					if historyNodes:
-						influenceJoints = []
-						for node in historyNodes:
-							if node.type() == 'joint':
-								influenceJoints.append(node)
-						tempNode = pm.duplicate(originNode,name=originNode+'_temp')[0]
-						pm.setAttr(tempNode+'.visibility',0)
-						pm.select(influenceJoints,r=True)
-						pm.select(tempNode,add=True)
-						pm.skinCluster(name=tempNode.name()+'_skinCluster',toSelectedBones=True,maximumInfluences=5,dropoffRate=4)
-						pm.copySkinWeights(originNode,tempNode,noMirror=True,surfaceAssociation='closestPoint',influenceAssociation='closestJoint',normalize=True)
-						pm.deltaMushToSkinCluster(source=originShapeNode.name(),target=tempNode.getShape().name())
-						pm.copySkinWeights(tempNode,originNode,noMirror=True,surfaceAssociation='closestPoint',influenceAssociation='closestJoint')
-						pm.delete(tempNode)
-	else:
-		pm.displayWarning('Please select polygon object!')
+    sels = pm.selected()
+    if not sels:
+        pm.displayWarning('Please select polygon object!')
+    for sel in sels:
+	if sel.getShape().type() != 'mesh':
+	    continue
+	originNode = sel
+	originShapeNodes = originNode.getShapes()
+	pm.select(cl=True)
+	if not originShapeNodes:
+	    continue
+	originShapeNode = originShapeNodes[0]
+	historyNodes = originShapeNode.listHistory()
+	if not historyNodes:
+	    continue
+	influenceJoints = []
+	for node in historyNodes:
+	    if node.type() == 'joint':
+	        influenceJoints.append(node)
+	tempNode = pm.duplicate(originNode,name=originNode+'_temp')[0]
+	pm.setAttr(tempNode+'.visibility',0)
+	pm.select(influenceJoints,r=True)
+	pm.select(tempNode,add=True)
+	pm.skinCluster(name=tempNode.name()+'_skinCluster',toSelectedBones=True,maximumInfluences=5,dropoffRate=4)
+	pm.copySkinWeights(originNode,tempNode,noMirror=True,surfaceAssociation='closestPoint',influenceAssociation='closestJoint',normalize=True)
+	pm.deltaMushToSkinCluster(source=originShapeNode.name(),target=tempNode.getShape().name())
+	pm.copySkinWeights(tempNode,originNode,noMirror=True,surfaceAssociation='closestPoint',influenceAssociation='closestJoint')
+	pm.delete(tempNode)
 
 def main():
 	batchCovert()
